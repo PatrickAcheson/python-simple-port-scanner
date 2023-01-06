a9a4923b6ce33f3d3f126ce04d0fd48981512048
patrick <patrick.acheson@hotmail.com>
2023-01-06 23:28:15 +0000

    updated

--- port-scan.py
+++ port-scan.py
@@ -27,54 +27,41 @@ def process(target_address, ports_open):
             try:
                 port_type = getservbyport(port,"tcp")
                 services.update({port:{"type":port_type,"status":"open"}})
-                print(f"{port}/tcp   {Fore.GREEN}{services[port]['status']}{Style.RESET_ALL}       {services[port]['type']}")
+                print(f"{port}/tcp   {Fore.GREEN}{services[port]['status']}{Style.RESET_ALL}    {services[port]['type']}")
             except:
                 services.update({port:{"type":"unknown","status":"open"}})
-                print(f"{port}/tcp   {Fore.GREEN}{services[port]['status']}{Style.RESET_ALL}       {services[port]['type']}")
+                print(f"{port}/tcp   {Fore.GREEN}{services[port]['status']}{Style.RESET_ALL}    {services[port]['type']}")
                 pass
 
-def help_prnt():
-    #A horrible attempt of implimenting a help menu I know *will fix this*
-    print("""Usage: python3 port-scan.py [flag] {target address} [threads]
-             OPTIONS:
-             -R: runs default mode using socket connection with TCP handshake
-             -H: Displays this mode lol""")
-    exit()
-
-if __name__== "__main__":
-    flag, target_address, threads = str(sys.argv[1]), str(sys.argv[2]), int(sys.argv[3])
-
-    if flag =="-R" or flag =="-r":
-        pass
-    elif flag == "-h" or flag == "-H":
-            help_prnt()          
-    else:
-        print("wrong syntax!")
-        help_prnt()
-
+if __name__ == "__main__":
+    try:
+        target_address, threads = str(sys.argv[1]), int(sys.argv[2])
     #Printing scan header
-    print("----------------------------------")
-    print(f"scan report for {target_address}  -bng-")
-    print("----------------------------------")
-    print("PORT      STATE      SERVICE")
+        print("----------------------------------")
+        print(f"scan report for {target_address}  -bng-")
+        print("----------------------------------")
+        print("PORT      STATE      SERVICE")
+        ports_open = []
+        thread_list = []
+        queue = Queue()
+        services = {}
+        ports_var = 1024 #To edit amount of a ports scanned edit this
+        port_list = range(1, ports_var)
+        process(target_address, ports_open)
+        use_queue(port_list)
 
-    ports_open = []
-    thread_list = []
-    queue = Queue()
-    services = {}
-    ports_var = 1024 #To edit amount of a ports scanned edit this
-
-    port_list = range(1, ports_var)
-    process(target_address, ports_open)
-    use_queue(port_list)
-
-    for t in range(threads):
-        thread = threading.Thread(target=process,args=(target_address, ports_open))
-        thread_list.append(thread)
-    for thread in thread_list:
-        thread.start()
-    for thread in thread_list:
-        thread.join()
-    print("----------------------------------")
-    print(f"hidden: {max(port_list) - len(services) + 1} closed tcp ports")
-    print(f"scan complete: on address ({target_address})")
+        for t in range(threads):
+            thread = threading.Thread(target=process,args=(target_address, ports_open))
+            thread_list.append(thread)
+        for thread in thread_list:
+            thread.start()
+        for thread in thread_list:
+            thread.join()
+        print("----------------------------------")
+        print(f"hidden: {max(port_list) - len(services) + 1} closed tcp ports")
+        print(f"scan complete: on address ({target_address})")
+    except Exception as err:
+        print(f"""
+        Usage: python3 script.py [IP address] [port range]
+        Example: python3 script.py 127.0.0.1 1000
+        Scans the range of ports from 1 to 1000 on the IP address 127.0.0.1""")
